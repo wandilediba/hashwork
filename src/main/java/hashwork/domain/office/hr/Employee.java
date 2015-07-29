a0fa05d4770b69d94a8b7ffbd35dd1cc36077a2f
zamashange <zamangwane.shange@gmail.com>
2015-07-29 21:41:32 +0200

    - HR and Payroll folders created - Employee class added to HR & Leave class added to Payroll

---
+++ src/main/java/hashwork/domain/office/hr/Employee.java
@@ -0,0 +1,10 @@
+package hashwork.domain.office.hr;
+
+import java.io.Serializable;
+
+/**
+ * Created by zamzam on 15/07/29.
+ */
+public class Employee implements Serializable {
+    private String id;
+}
---
+++ src/main/java/hashwork/domain/office/payroll/Leave.java
@@ -0,0 +1,10 @@
+package hashwork.domain.office.payroll;
+
+import java.io.Serializable;
+
+/**
+ * Created by zamzam on 15/07/29.
+ */
+public class Leave implements Serializable {
+    private String id;
+}
