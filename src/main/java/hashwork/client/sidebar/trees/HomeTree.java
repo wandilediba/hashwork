0b506755f2943530f3d1e5ee19f515e8d88aaee3
hashcode <boniface@hashcode.zm>
2015-08-17 21:24:23 +0200

    Building App Side Bar

---
+++ src/main/java/hashwork/client/sidebar/trees/HomeTree.java
@@ -0,0 +1,21 @@
+package hashwork.client.sidebar.trees;
+
+import com.vaadin.event.ItemClickEvent;
+import hashwork.client.content.MainLayout;
+
+/**
+ * Created by hashcode on 2015/08/17.
+ */
+public class HomeTree implements ItemClickEvent.ItemClickListener {
+    private final MainLayout main;
+    private static final String LANDING_TAB = "LANDING";
+
+    public HomeTree(MainLayout main) {
+        this.main = main;
+    }
+
+    @Override
+    public void itemClick(ItemClickEvent event) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
