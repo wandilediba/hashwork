04668edcdfbe251d5332452599cc9ce906f28ed8
zamashange <zamangwane.shange@gmail.com>
2015-08-19 22:11:06 +0200

    - domain/payroll/core/deductions objects completed

--- src/main/java/hashwork/domain/payroll/core/deductions/EmployeeLeaveData.java
+++ src/main/java/hashwork/domain/payroll/core/deductions/EmployeeLeaveData.java
@@ -15,4 +15,122 @@ public class EmployeeLeaveData implements Serializable {
     private boolean paidLeave;
     private int daysTaken;
     private Date commencementDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPersonId() {
+        return personId;
+    }
+
+    public String getLeaveType() {
+        return leaveType;
+    }
+
+    public boolean isPaidLeave() {
+        return paidLeave;
+    }
+
+    public int getDaysTaken() {
+        return daysTaken;
+    }
+
+    public Date getCommencementDate() {
+        return commencementDate;
+    }
+
+    private EmployeeLeaveData(){
+
+    }
+
+    private EmployeeLeaveData(Builder builder){
+        this.id = builder.id;
+        this.personId = builder.personId;
+        this.leaveType = builder.leaveType;
+        this.paidLeave = builder.paidLeave;
+        this.daysTaken = builder.daysTaken;
+        this.commencementDate = builder.commencementDate;
+    }
+
+    public static class Builder{
+        private String id;
+        private String personId;
+        private String leaveType;
+        private boolean paidLeave;
+        private int daysTaken;
+        private Date commencementDate;
+
+        public Builder id(String value){
+            this.id = value;
+            return this;
+        }
+
+        public Builder personId(String value) {
+            this.personId = value;
+            return this;
+        }
+
+        public Builder leaveType(String value) {
+            this.leaveType = value;
+            return this;
+        }
+
+        public Builder paidLeave(boolean value) {
+            this.paidLeave = value;
+            return this;
+        }
+
+        public Builder daysTaken(int value) {
+            this.daysTaken = value;
+            return this;
+        }
+
+        public Builder commencementDate(Date value) {
+            this.commencementDate = value;
+            return this;
+        }
+
+        public Builder copy(EmployeeLeaveData value) {
+            this.id = value.id;
+            this.personId = value.personId;
+            this.leaveType = value.leaveType;
+            this.paidLeave = value.paidLeave;
+            this.daysTaken = value.daysTaken;
+            this.commencementDate = value.commencementDate;
+            return this;
+        }
+        public EmployeeLeaveData build(){return new EmployeeLeaveData(this);}
+
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof EmployeeLeaveData)) return false;
+
+        EmployeeLeaveData that = (EmployeeLeaveData) o;
+
+        if (isPaidLeave() != that.isPaidLeave()) return false;
+        if (getDaysTaken() != that.getDaysTaken()) return false;
+        if (getId() != null ? !getId().equals(that.getId()) : that.getId() != null) return false;
+        if (getPersonId() != null ? !getPersonId().equals(that.getPersonId()) : that.getPersonId() != null)
+            return false;
+        if (getLeaveType() != null ? !getLeaveType().equals(that.getLeaveType()) : that.getLeaveType() != null)
+            return false;
+        return !(getCommencementDate() != null ? !getCommencementDate().equals(that.getCommencementDate()) : that.getCommencementDate() != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getPersonId() != null ? getPersonId().hashCode() : 0);
+        result = 31 * result + (getLeaveType() != null ? getLeaveType().hashCode() : 0);
+        result = 31 * result + (isPaidLeave() ? 1 : 0);
+        result = 31 * result + getDaysTaken();
+        result = 31 * result + (getCommencementDate() != null ? getCommencementDate().hashCode() : 0);
+        return result;
+    }
 }
--- src/main/java/hashwork/domain/payroll/core/deductions/EmployeeUIFContribution.java
+++ src/main/java/hashwork/domain/payroll/core/deductions/EmployeeUIFContribution.java
@@ -15,4 +15,114 @@ public class EmployeeUIFContribution implements Serializable {
     private BigDecimal contribution;
     private BigDecimal currentAmount;
     private Date lastContributionDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPersonId() {
+        return personId;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public BigDecimal getContribution() {
+        return contribution;
+    }
+
+    public BigDecimal getCurrentAmount() {
+        return currentAmount;
+    }
+
+    public Date getLastContributionDate() {
+        return lastContributionDate;
+    }
+
+    private EmployeeUIFContribution(){}
+
+    private EmployeeUIFContribution(Builder builder){
+        this.id = builder.id;
+        this.personId = builder.personId;
+        this.country = builder.country;
+        this.contribution = builder.contribution;
+        this.currentAmount = builder.currentAmount;
+        this.lastContributionDate = builder.lastContributionDate;
+    }
+
+    public static class Builder{
+        private String id;
+        private String personId;
+        private String country;
+        private BigDecimal contribution;
+        private BigDecimal currentAmount;
+        private Date lastContributionDate;
+
+        public Builder id(String value){
+            this.id = value;
+            return this;
+        }
+        public Builder personaId(String value){
+            this.personId = value;
+            return this;
+        }
+        public Builder country(String value){
+            this.country = value;
+            return this;
+        }
+        public Builder contribution(BigDecimal value){
+            this.contribution = value;
+            return this;
+        }
+        public Builder currentAmount(BigDecimal value){
+            this.currentAmount = value;
+            return this;
+        }
+        public Builder lastContributionDate(Date value){
+            this.lastContributionDate = value;
+            return this;
+        }
+
+        public Builder copy(EmployeeUIFContribution value) {
+            this.id = value.id;
+            this.personId = value.personId;
+            this.country = value.country;
+            this.contribution = value.contribution;
+            this.currentAmount = value.currentAmount;
+            this.lastContributionDate = value.lastContributionDate;
+            return this;
+        }
+        public EmployeeUIFContribution build(){return new EmployeeUIFContribution(this);}
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof EmployeeUIFContribution)) return false;
+
+        EmployeeUIFContribution that = (EmployeeUIFContribution) o;
+
+        if (getId() != null ? !getId().equals(that.getId()) : that.getId() != null) return false;
+        if (getPersonId() != null ? !getPersonId().equals(that.getPersonId()) : that.getPersonId() != null)
+            return false;
+        if (getCountry() != null ? !getCountry().equals(that.getCountry()) : that.getCountry() != null) return false;
+        if (getContribution() != null ? !getContribution().equals(that.getContribution()) : that.getContribution() != null)
+            return false;
+        if (getCurrentAmount() != null ? !getCurrentAmount().equals(that.getCurrentAmount()) : that.getCurrentAmount() != null)
+            return false;
+        return !(getLastContributionDate() != null ? !getLastContributionDate().equals(that.getLastContributionDate()) : that.getLastContributionDate() != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getPersonId() != null ? getPersonId().hashCode() : 0);
+        result = 31 * result + (getCountry() != null ? getCountry().hashCode() : 0);
+        result = 31 * result + (getContribution() != null ? getContribution().hashCode() : 0);
+        result = 31 * result + (getCurrentAmount() != null ? getCurrentAmount().hashCode() : 0);
+        result = 31 * result + (getLastContributionDate() != null ? getLastContributionDate().hashCode() : 0);
+        return result;
+    }
 }
