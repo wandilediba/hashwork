19d128b8fc1cfbf02164a81ab99f19bf90c9213f
zamashange <zamangwane.shange@gmail.com>
2015-08-20 19:21:42 +0200

    - GenderList class updated

--- src/main/java/hashwork/domain/ui/demographics/GenderList.java
+++ src/main/java/hashwork/domain/ui/demographics/GenderList.java
@@ -9,4 +9,41 @@ public class GenderList implements Serializable {
     private static final long serialVersionUID = 1L;
     private String id;
     private String gender;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    private GenderList(Builder builder){
+        this.id = builder.id;
+        this.gender = builder.gender;
+    }
+
+    public static class Builder{
+        private String id;
+        private String gender;
+
+        public Builder id(String value){
+            this.id = value;
+            return this;
+        }
+        public Builder gender(String value){
+            this.gender = value;
+            return this;
+        }
+
+        public Builder copy(GenderList value) {
+            this.id = value.id;
+            this.gender = value.gender;
+            return this;
+        }
+
+        public GenderList build(){
+            return new GenderList(this);
+        }
+    }
 }
