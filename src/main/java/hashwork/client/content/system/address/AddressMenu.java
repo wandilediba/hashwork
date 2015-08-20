8585e392d3a0885c9cb7a29de6c97a9cd67f9f20
Garran <Garran@Garran-PC>
2015-08-20 21:36:42 +0200

    Added: AddressMenu CountryMenu EmployeeMenu RegionMenu

---
+++ src/main/java/hashwork/client/content/system/address/AddressMenu.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashwork.client.content.system.address;
+
+import com.vaadin.ui.TabSheet;
+import com.vaadin.ui.VerticalLayout;
+import hashwork.client.content.MainLayout;
+
+/**
+ *
+ * @author Garran
+ */
+public class AddressMenu extends VerticalLayout {
+    private final MainLayout main;
+    private TabSheet tab;
+    
+    public AddressMenu(MainLayout main, String tab) {
+        this.main = main;
+    }
+}
