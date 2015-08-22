32f4cfa4bee2a396462a77143dd6ce9ed12cef5f
hashcode <boniface@hashcode.zm>
2015-08-22 20:22:07 +0200

    Added Respository and Added a Ketgenerator to Demographics factory

--- src/main/java/hashwork/factories/ui/demographics/MaritalStatusListFactory.java
+++ src/main/java/hashwork/factories/ui/demographics/MaritalStatusListFactory.java
@@ -1,13 +1,16 @@
 package hashwork.factories.ui.demographics;
 
+import hashwork.app.util.KeyGenerator;
 import hashwork.domain.ui.demographics.MaritalStatusList;
 
 /**
  * Created by zamzam on 15/08/22.
  */
 public class MaritalStatusListFactory {
-    public static MaritalStatusList getMaritalStatusList(String statusName){
-        MaritalStatusList maritalStatusList = new MaritalStatusList.Builder()
+    public static MaritalStatusList getMaritalStatusList(String statusName) {
+        MaritalStatusList maritalStatusList = new MaritalStatusList
+                .Builder()
+                .id(KeyGenerator.getEntityId())
                 .statusName(statusName).build();
         return maritalStatusList;
     }
--- src/main/java/hashwork/factories/ui/demographics/RolesListFactory.java
+++ src/main/java/hashwork/factories/ui/demographics/RolesListFactory.java
@@ -1,13 +1,17 @@
 package hashwork.factories.ui.demographics;
 
+import hashwork.app.util.KeyGenerator;
 import hashwork.domain.ui.demographics.RolesList;
 
 /**
  * Created by zamzam on 15/08/22.
  */
 public class RolesListFactory {
-    public static RolesList getRolesList(String roleName, String description){
-        RolesList rolesList = new RolesList.Builder().roleName(roleName)
+    public static RolesList getRolesList(String roleName, String description) {
+        RolesList rolesList = new RolesList
+                .Builder()
+                .id(KeyGenerator.getEntityId())
+                .roleName(roleName)
                 .description(description)
                 .build();
         return rolesList;
