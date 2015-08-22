58f4b41b4ccd6048da9100b6d07ab09fe188a0e2
zamashange <zamangwane.shange@gmail.com>
2015-08-22 19:24:32 +0200

    demographicsFactory complete

--- src/main/java/hashwork/factories/ui/demographics/GenderListFactory.java
+++ src/main/java/hashwork/factories/ui/demographics/GenderListFactory.java
@@ -1,7 +1,14 @@
 package hashwork.factories.ui.demographics;
 
+import hashwork.domain.ui.demographics.GenderList;
+
 /**
  * Created by hashcode on 2015/08/16.
  */
 public class GenderListFactory {
+    public static GenderList getGenderList(String gender){
+        GenderList genderList = new GenderList.Builder()
+                .gender(gender).build();
+        return genderList;
+    }
 }
