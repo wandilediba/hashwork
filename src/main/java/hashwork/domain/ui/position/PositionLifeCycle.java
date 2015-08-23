55a82a57fc9e3d53764f7e1ebae01033603a0beb
wandilediba <wandile62@gmail.com>
2015-08-23 22:07:50 +0200

    updated ui/position

--- src/main/java/hashwork/domain/ui/position/PositionLifeCycle.java
+++ src/main/java/hashwork/domain/ui/position/PositionLifeCycle.java
@@ -12,4 +12,83 @@ public class PositionLifeCycle implements Serializable {
     private String positionId;
     private Date dateofAction;
     private String status;
+
+
+
+public PositionLifeCycle(){}
+
+    public static class Builder{
+        private String id;
+        private String positionId;
+        private Date dateofAction;
+        private String status;
+
+        public Builder id(String value){
+            this.id = value;
+            return this;
+        }
+
+
+        public Builder positionId( String value){
+            this.positionId = value;
+            return this;
+        }
+
+        public Builder dateofAction(Date value){
+            this.dateofAction = value;
+            return this;
+        }
+
+        public Builder status( String value){
+            this.status = value;
+            return this;
+        }
+
+        public Builder copy(PositionLifeCycle value) {
+            this.id = value.Id;
+            this.dateofAction = value.dateofAction;
+            this.status = value.status;
+            this.positionId = value.positionId;
+            return this;
+        }
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public String getPositionId() {
+        return positionId;
+    }
+
+    public Date getDateofAction() {
+        return dateofAction;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PositionLifeCycle that = (PositionLifeCycle) o;
+
+        if (!Id.equals(that.Id)) return false;
+        if (!positionId.equals(that.positionId)) return false;
+        if (!dateofAction.equals(that.dateofAction)) return false;
+        return status.equals(that.status);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Id.hashCode();
+        result = 31 * result + positionId.hashCode();
+        result = 31 * result + dateofAction.hashCode();
+        result = 31 * result + status.hashCode();
+        return result;
+    }
 }
