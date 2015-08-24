519882216bd88f213af1e85c60d4debc2627cc6b
hashcode <boniface@hashcode.zm>
2015-08-24 10:40:50 +0200

    Completed RoleList Except the Update Part and added some sample Data to Table but Map is not persistent so you will see some NPEs

--- src/main/java/hashwork/repository/ui/demographics/Impl/RolesListRepositoryImpl.java
+++ src/main/java/hashwork/repository/ui/demographics/Impl/RolesListRepositoryImpl.java
@@ -1,10 +1,11 @@
 package hashwork.repository.ui.demographics.Impl;
 
+import hashwork.app.conf.Connection;
 import hashwork.domain.ui.demographics.RolesList;
-import hashwork.factories.ui.demographics.RolesListFactory;
 import hashwork.repository.ui.demographics.RolesListRepository;
+import org.redisson.Redisson;
+import org.redisson.core.RMap;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -13,45 +14,35 @@ import java.util.Set;
  * Created by hashcode on 2015/08/22.
  */
 public class RolesListRepositoryImpl implements RolesListRepository {
-    Map<String, RolesList> lists = new HashMap<>();
-    RolesList rol1 = RolesListFactory.getRolesList("ADMIN", "System Admin");
-
-    RolesList rol2 = RolesListFactory.getRolesList("EMPLOYEE", "Employee");
-
-    RolesList rol3 = RolesListFactory.getRolesList("APPLICATING", "APPLICANT");
-
-    public RolesListRepositoryImpl() {
-        lists.put(rol1.getId(), rol1);
-        lists.put(rol2.getId(), rol2);
-        lists.put(rol3.getId(), rol3);
-    }
+    Redisson redisson = Connection.getCinnection();
+    RMap<String, RolesList> rolelist = redisson.getMap("rolelist");
 
     @Override
     public RolesList findById(String s) {
-        return lists.get(s);
+        return rolelist.get(s);
     }
 
     @Override
     public RolesList save(RolesList entity) {
-        lists.put(entity.getId(), entity);
+        rolelist.put(entity.getId(), entity);
         return entity;
     }
 
     @Override
     public RolesList update(RolesList entity) {
-        lists.put(entity.getId(), entity);
+        rolelist.put(entity.getId(), entity);
         return entity;
     }
 
     @Override
     public void delete(RolesList entity) {
-        lists.remove(entity.getId());
+        rolelist.remove(entity.getId());
     }
 
     @Override
     public Set<RolesList> findAll() {
         Set<RolesList> set = new HashSet<>();
-        for (Map.Entry<String, RolesList> entry : lists.entrySet()) {
+        for (Map.Entry<String, RolesList> entry : rolelist.entrySet()) {
             set.add(entry.getValue());
         }
         return set;
