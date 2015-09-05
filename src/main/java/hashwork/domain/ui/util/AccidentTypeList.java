d7958402536428379f04d35c50f6d118e1ec28a3
wandilediba <wandile62@gmail.com>
2015-09-05 16:41:49 +0200

    UI/util done

--- src/main/java/hashwork/domain/ui/util/AccidentTypeList.java
+++ src/main/java/hashwork/domain/ui/util/AccidentTypeList.java
@@ -64,5 +64,6 @@ public class AccidentTypeList implements Serializable {
 
 
 
+
     }
 }
--- src/main/java/hashwork/domain/ui/util/Currency.java
+++ src/main/java/hashwork/domain/ui/util/Currency.java
@@ -12,4 +12,70 @@ public class Currency implements Serializable {
     private String code;
     private String name;
     private String symbol;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    private Currency(Builder builder){
+        this.id = builder.id;
+        this.code = builder.code;
+        this.name = builder.name;
+        this.symbol = builder.symbol;
+    }
+
+    public static class Builder {
+
+        private String id;
+        private String code;
+        private String name;
+        private String symbol;
+
+        public Builder id(String value){
+            this.id = value;
+            return this;
+        }
+
+        public Builder code(String value){
+            this.code = value;
+            return this;
+        }
+
+        public Builder name(String value){
+            this.name = value;
+            return this;
+        }
+
+        public Builder symbol(String value){
+            this.symbol = value;
+            return this;
+        }
+
+        public Builder copy(Currency value) {
+            this.id = value.id;
+            this.code = value.code;
+            this.name = value.name;
+            this.symbol = value.symbol;
+
+            return this;
+        }
+
+        public Currency build(){
+            return new Currency(this);
+        }
+
+
+    }
 }
