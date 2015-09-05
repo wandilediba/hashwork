023906a5b97cc47a6ccb745ac1fdb5f90063274a
wandilediba <wandile62@gmail.com>
2015-09-05 15:43:01 +0200

    UI/util done

--- src/main/java/hashwork/domain/ui/util/AccidentTypeList.java
+++ src/main/java/hashwork/domain/ui/util/AccidentTypeList.java
@@ -11,4 +11,58 @@ public class AccidentTypeList implements Serializable {
     private String  name;
     private String description;
 
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    private AccidentTypeList(Builder builder){
+        this.id = builder.id;
+        this.name = builder.name;
+        this.description = builder.description;
+
+    }
+
+    public static class Builder {
+
+        private String id;
+        private String  name;
+        private String description;
+
+        public Builder id(String value){
+            this.id = value;
+            return this;
+        }
+
+        public Builder name(String value){
+            this.name = value;
+            return this;
+        }
+
+        public Builder description(String value){
+            this.description = value;
+            return this;
+        }
+
+        public Builder copy(AccidentTypeList value){
+            this.id = value.id;
+            this.name = value.name;
+            this.description = value.description;
+            return this;
+
+        }
+        public AccidentTypeList build(){
+            return new AccidentTypeList(this);
+        }
+
+
+
+    }
 }
