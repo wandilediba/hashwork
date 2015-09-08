82f6b08f76ae2aa3c454e5e51380cf276c9d834a
zamashange <zamangwane.shange@gmail.com>
2015-09-08 16:18:59 +0200

    -payroll repository setup

--- src/main/java/hashwork/factories/payroll/ui/Job/BenefitTypeFactory.java
+++ src/main/java/hashwork/factories/payroll/ui/Job/BenefitTypeFactory.java
@@ -1,17 +1,23 @@
-package hashwork.factories.payroll.ui.job;
+package hashwork.factories.payroll.ui.Job;
+
+import hashwork.app.util.KeyGenerator;
+import hashwork.domain.payroll.ui.job.BenefitType;
+
+import java.math.BigDecimal;
+import java.util.Map;
 
 /**
  * Created by zamzam on 15/09/06.
  */
 public class BenefitTypeFactory {
-//    public static BenefitType getBenefitType(Map<String,String> values,
-//                                             BigDecimal amount){
-//        BenefitType benefitType = new BenefitType
-//                .Builder()
-//                .id(KeyGenerator.getEntityId())
-//                .benefitTypeName(values.get("benefitTypeName"))
-//                .amount(amount)
-//                .currencyId(values.get("currenyId")).build();
-//        return benefitType;
-//    }
+    public static BenefitType getBenefitType(Map<String,String> values,
+                                             BigDecimal amount){
+        BenefitType benefitType = new BenefitType
+                .Builder()
+                .id(KeyGenerator.getEntityId())
+                .benefitTypeName(values.get("benefitTypeName"))
+                .amount(amount)
+                .currencyId(values.get("currencyId")).build();
+        return benefitType;
+    }
 }
---
+++ src/main/java/hashwork/services/payroll/ui/EarningListService.java
@@ -0,0 +1,10 @@
+package hashwork.services.payroll.ui;
+
+import hashwork.domain.payroll.ui.EarningsList;
+import hashwork.services.Services;
+
+/**
+ * Created by zamzam on 15/09/08.
+ */
+public interface EarningListService extends Services<EarningsList,String> {
+}
