cdd0eb0ab3c0014180835671aadb1dcd04b147b6
zamashange <zamangwane.shange@gmail.com>
2015-09-09 18:02:45 +0200

    -repository/payroll/ui/deductions updated

---
+++ src/main/java/hashwork/repository/payroll/ui/deductions/Impl/PensionFundRepositoryImpl.java
@@ -0,0 +1,49 @@
+package hashwork.repository.payroll.ui.deductions.Impl;
+
+import hashwork.domain.payroll.ui.deductions.PensionFund;
+import hashwork.repository.payroll.ui.deductions.PensionFundRepository;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by zamzam on 15/09/09.
+ */
+public class PensionFundRepositoryImpl implements PensionFundRepository {
+    Map<String, PensionFund> lists = new HashMap<>();
+
+    public PensionFundRepositoryImpl() {
+
+    }
+
+    @Override
+    public PensionFund findById(String s) {
+        return lists.get(s);
+    }
+
+    @Override
+    public PensionFund save(PensionFund entity) {
+        return lists.put(entity.getId(), entity);
+    }
+
+    @Override
+    public PensionFund update(PensionFund entity) {
+        return lists.put(entity.getId(), entity);
+    }
+
+    @Override
+    public void delete(PensionFund entity) {
+        lists.remove(entity.getId());
+    }
+
+    @Override
+    public Set<PensionFund> findAll() {
+        Set<PensionFund> set = new HashSet<>();
+        for (Map.Entry<String, PensionFund> entry : lists.entrySet()) {
+            set.add(entry.getValue());
+        }
+        return set;
+    }
+}
