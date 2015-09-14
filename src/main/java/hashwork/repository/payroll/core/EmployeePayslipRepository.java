00a95bfa555580ea8e7ae560476979c16531e964
Your Name <you@example.com>
2015-09-14 14:09:07 +0200

    Added service, & repo for EmployeePayslip

---
+++ src/main/java/hashwork/repository/payroll/core/EmployeePayslipRepository.java
@@ -0,0 +1,10 @@
+package hashwork.repository.payroll.core;
+
+import hashwork.repository.CrudRepository;
+
+/**
+ * Created by nkosi on 2015/09/14.
+ */
+public interface EmployeePayslipRepository extends CrudRepository<EmployeePayslipRepository, String> {
+
+}
---
+++ src/main/java/hashwork/services/payroll/core/EmployeePayslipService.java
@@ -0,0 +1,10 @@
+package hashwork.services.payroll.core;
+
+import hashwork.domain.payroll.core.EmployeePayslip;
+import hashwork.services.Services;
+
+/**
+ * Created by nkosi on 2015/09/14.
+ */
+public interface EmployeePayslipService  extends Services<EmployeePayslip, String> {
+}
