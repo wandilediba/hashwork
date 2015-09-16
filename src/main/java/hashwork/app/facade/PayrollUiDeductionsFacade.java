ea065b9114619c3c11b264e96fdc667a8d40cc44
Your Name <you@example.com>
2015-09-16 22:08:05 +0200

    working on PayrollUiDeductionsFacade waiting  serivices in that directory

---
+++ src/main/java/hashwork/app/facade/PayrollUiDeductionsFacade.java
@@ -0,0 +1,12 @@
+package hashwork.app.facade;
+
+import hashwork.services.payroll.ui.deductions.AllowanceTypeListService;
+import hashwork.services.payroll.ui.deductions.Impl.AllowanceTypeListServiceImpl;
+
+/**
+ * Created by nkosi on 2015/09/16.
+ */
+public class PayrollUiDeductionsFacade {
+    public final static AllowanceTypeListService allowanceTypeListService = new AllowanceTypeListServiceImpl();
+    
+}
