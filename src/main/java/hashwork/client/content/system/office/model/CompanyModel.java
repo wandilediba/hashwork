abbce2b5deefaea18bd9626869d45793d8476407
garran <garranmichaels@gmail.com>
2015-09-20 21:33:47 +0200

    Added Office / model

---
+++ src/main/java/hashwork/client/content/system/office/model/CompanyModel.java
@@ -0,0 +1,18 @@
+package hashwork.client.content.system.office.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by garran on 2015/09/20.
+ */
+public class CompanyModel implements Serializable {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
